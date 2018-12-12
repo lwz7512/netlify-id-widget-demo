6ba09766c4ae5f0e97de0ccdec2e1ac2c25c23ad
lwz7512 <lwz7512@gmail.com>
2018-12-12 09:58:54 -0400

    first commit...

--- README.md
+++ README.md
@@ -1,44 +1,32 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+### Additional modules included
 
-In the project directory, you can run:
+* @reach/router
+* netlify-identity-widget
 
-### `npm start`
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+### Installation
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+> $ yarn
 
-### `npm test`
+### Run
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+> $ yarn start
 
-### `npm run build`
+### Prerequisites
 
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
+a netlify account, and,
 
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
+a site app setup in [netlify](https://app.netlify.com/), and an API endpoint needed while locally running. The endpoint can be obtained from your netlify site `Identity` page.
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `npm run eject`
+### Online Demo
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+[here]()
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+### Others?
 
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+currently no, have fun...
 
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
--- src/App.js
+++ src/App.js
@@ -1,28 +1,144 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+import { Router, Link, navigate } from "@reach/router"
+import netlifyIdentity from 'netlify-identity-widget';
+
+
+let Home = () => (
+  <div>
+    <h1>Home</h1>
+    <nav>
+      <Link to="/">Home</Link> |{" "}
+      <Link to="/dashboard">Dashboard</Link>
+    </nav>
+  </div>
+)
+
+let Dash = () => <div>
+    <h1>Dashboard</h1> 
+    <Link to="/">Home</Link>
+  </div>
+
+
+class Homeboard extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       user: null
+    }
+  }
+
+  componentDidMount() {
+    const user = netlifyIdentity.currentUser();
+    console.log({ user });
+    if(user) this.setState({user})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Home</h1>
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/dashboard">Dashboard</Link>
+        </nav>
+        {this.state.user?<p>I m {this.state.user.user_metadata.full_name}</p>:false}
       </div>
-    );
+    )
   }
+  
+
+}
+
+class Dashboard extends Component {
+
+  // rcon
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       user: null
+    }
+
+  }
+
+  // cdm down enter
+  componentDidMount() {
+    const user = netlifyIdentity.currentUser();
+    console.log({ user });
+    if(user) this.setState({user})
+  }
+  
+  
+  login = () => {
+    netlifyIdentity.ctx = this
+    console.log('loging...');
+    netlifyIdentity.open()
+    netlifyIdentity.on('login', user => {
+      console.log('user got!', user)
+      netlifyIdentity.ctx.setState({user})
+      netlifyIdentity.close()
+      setTimeout(()=>navigate('/'), 400);
+    })
+  }
+
+  logout = () => {
+    netlifyIdentity.ctx = this
+    console.log('logout...');
+    netlifyIdentity.logout();
+    netlifyIdentity.on('logout', () => {
+      console.log('user logout!')
+      netlifyIdentity.ctx.setState({user:null})
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Link to="/">Home</Link>
+        <h1>Dashboard</h1> 
+        {this.state.user?
+          (
+            <div>
+            <p>Welcome: {this.state.user.user_metadata.full_name}</p>
+            <button onClick={this.logout}>Log out</button>
+           </div>
+          ):
+          (<button onClick={this.login}>Log in</button>)
+        }
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+
+  // rcon
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+    netlifyIdentity.init();
+    console.log('nli init...')
+  }
+  
+
+  render() {
+    return (
+      <Router>
+        <Homeboard path="/" />
+        <Dashboard path="dashboard" />
+      </Router>
+    )
+  }
+
 }
 
 export default App;
